b6fc53e64611b0fa1e1956127e3b501c4585acf2
Parsa Ghezelbash <119513155+ParsaGhezelbash@users.noreply.github.com>
2025-10-22 00:28:33 +0330

    bug fix

--- data_rater.py
+++ data_rater.py
@@ -303,7 +303,7 @@ def compute_rate(config: DataRaterConfig, model, data_rater, test_loader):
             loss_fn = nn.CrossEntropyLoss(reduction="none") if config.loss_type == 'cross_entropy' else nn.MSELoss(reduction="none")
             preds = model(batch_samples)
             losses = loss_fn(preds, batch_labels).cpu().numpy()
-            print(losses.shape.cpu().numpy(), scores.cpu().numpy().shape)
+            print(losses.cpu().numpy().shape, scores.cpu().numpy().shape)
             loss_values.extend(losses.cpu().numpy())
             print((preds.argmax(dim=-1) == batch_labels).cpu().numpy().shape)
             accuracy_values.extend((preds.argmax(dim=-1) == batch_labels).cpu().numpy())
